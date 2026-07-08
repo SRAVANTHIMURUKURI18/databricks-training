b2ccbecf7385eb6e77c62612ebecccad331dac75
Sravanthi_Murukuri <160226421+SRAVANTHIMURUKURI18@users.noreply.github.com>
2026-07-08 19:46:50 +0530

    Update phase2.sql

--- phase2/phase2.sql
+++ phase2/phase2.sql
@@ -1,68 +1,81 @@
+-- ==========================================
+-- Phase 2 - SQL Queries
+-- ==========================================
+
 -- 1. Total order amount for each customer
+
 SELECT
     customer_id,
-    SUM(amount) AS total_amount
-FROM orders
+    SUM(total_amount) AS total_order_amount
+FROM sales
 GROUP BY customer_id;
 
-----------------------------------------------------
+--------------------------------------------------
 
 -- 2. Top 3 customers by total spend
+
 SELECT
     customer_id,
-    SUM(amount) AS total_spend
-FROM orders
+    SUM(total_amount) AS total_spend
+FROM sales
 GROUP BY customer_id
 ORDER BY total_spend DESC
 LIMIT 3;
 
-----------------------------------------------------
+--------------------------------------------------
 
 -- 3. Customers with no orders
+
 SELECT
     c.customer_id,
-    c.customer_name
+    c.first_name,
+    c.last_name
 FROM customers c
-LEFT JOIN orders o
-ON c.customer_id = o.customer_id
-WHERE o.customer_id IS NULL;
+LEFT JOIN sales s
+ON c.customer_id = s.customer_id
+WHERE s.customer_id IS NULL;
 
-----------------------------------------------------
+--------------------------------------------------
 
 -- 4. City-wise total revenue
+
 SELECT
     c.city,
-    SUM(o.amount) AS revenue
+    SUM(s.total_amount) AS revenue
 FROM customers c
-JOIN orders o
-ON c.customer_id = o.customer_id
-GROUP BY c.city;
+JOIN sales s
+ON c.customer_id = s.customer_id
+GROUP BY c.city
+ORDER BY revenue DESC;
 
-----------------------------------------------------
+--------------------------------------------------
 
 -- 5. Average order amount per customer
+
 SELECT
     customer_id,
-    AVG(amount) AS average_amount
-FROM orders
+    AVG(total_amount) AS average_order_amount
+FROM sales
 GROUP BY customer_id;
 
-----------------------------------------------------
+--------------------------------------------------
 
 -- 6. Customers with more than one order
+
 SELECT
     customer_id,
-    COUNT(*) AS total_orders
-FROM orders
+    COUNT(sale_id) AS total_orders
+FROM sales
 GROUP BY customer_id
-HAVING COUNT(*) > 1;
+HAVING COUNT(sale_id) > 1;
 
-----------------------------------------------------
+--------------------------------------------------
 
 -- 7. Sort customers by total spend descending
+
 SELECT
     customer_id,
-    SUM(amount) AS total_spend
-FROM orders
+    SUM(total_amount) AS total_spend
+FROM sales
 GROUP BY customer_id
 ORDER BY total_spend DESC;
